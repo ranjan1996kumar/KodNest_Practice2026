00f5822793cf774175fcc7ee90f08f2d0568bb1f
RANJAN KUMAR <ranjan1996kumar@gmail.com>
2026-03-06 22:19:24 +0530

    updates

--- Oops_Practice/src/Program.java
+++ Oops_Practice/src/Program.java
@@ -1,4 +1,33 @@
+class Program {
+    static int a;
 
-public class Program {
+    static
+    {
+        System.out.println("In the static block");
+        a = 10;
+    }
 
+    static void display1() {
+        System.out.println("In the static method");
+        System.out.println(a);
+    }
+
+    int b;
+
+    {
+        System.out.println("In the non static block");
+        b = 20;
+    }
+
+    void display2() {
+        System.out.println("In the non static method");
+        System.out.println(b);
+    }
+
+    public static void main(String[] args)
+    {
+        Program.display1();
+        Program p = new Program();
+        p.display2();
+    }
 }
